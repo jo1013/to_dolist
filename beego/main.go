08ecfe931e22342b8e5c50513673e1882c425c8f
sawyer-jo <tkdah0@gmail.com>
2024-04-17 14:17:48 +0900

    react add

--- beego/main.go
+++ beego/main.go
@@ -1,30 +1,25 @@
-package main
-
 import (
+	"os"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	_ "github.com/go-sql-driver/mysql" // MySQL driver import
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	orm.RegisterDataBase("default", "mysql", "user:password@tcp(localhost:3306)/dbname?charset=utf8")
-	orm.RunSyncdb("default", false, true)
+	// 환경 변수에서 데이터베이스 연결 정보 읽기
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 
-	// Enable session management
+	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8"
+	orm.RegisterDataBase("default", "mysql", dsn)
+
+	orm.RunSyncdb("default", false, true)
 	beego.BConfig.WebConfig.Session.SessionOn = true
 }
-
-// func init() {
-// 	// Initialize the database settings
-// 	orm.RegisterDriver("sqlite3", orm.DRSqlite)
-// 	orm.RegisterDataBase("default", "sqlite3", "file:data.db?cache=shared&mode=rwc", 30)
-
-// 	// Enable automatic database migration
-// 	orm.RunSyncdb("default", false, true)
-
-// 	// Enable session management
-// 	beego.BConfig.WebConfig.Session.SessionOn = true
-// }
 
 func main() {
 	beego.Run()
